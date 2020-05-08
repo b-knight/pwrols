38af70e5c0a315270145dc23f88265be0b4ff66b
Benjamin Knight <b-knight@users.noreply.github.com>
2020-05-07 21:45:38 -0700

    Update extract_rsqr_delta.py

--- pwrols/extract_rsqr_delta.py
+++ pwrols/extract_rsqr_delta.py
@@ -20,14 +20,18 @@ def extract_r_delta(df, dv, t, iv = None):
     Y = df[dv]
     
     if iv is not None:
+        X2 = df[iv]
+        X2 = sm.add_constant(X2)
         iv.append(t)
         X1 = df[iv]
+        X1 = sm.add_constant(X1)
     else:
         X1 = df[t]
-    X1 = sm.add_constant(X1)
-    X2 = X1['const']
-    
+        X1 = sm.add_constant(X1)
+        X2 = X1['const']
+
     model1 = sm.OLS(Y.astype(float), X1.astype(float)).fit()
+    print("With treatment, the estimated r-squared is {}.".format(model1.rsquared))
     model2 = sm.OLS(Y.astype(float), X2.astype(float)).fit()
-    
+    print("Without treatment, the estimated r-squared is {}.".format(model2.rsquared))
     return max(model1.rsquared, 0) - max(model2.rsquared, 0) 
